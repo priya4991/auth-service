97bb599b251505b4bd5d64092fbb8f9cd32350c5
Priyadarshini Dutta <priyadarshinidutta@PriyadahinisAir.lan>
2023-07-05 22:16:19 +0100

    renamed service

---
+++ recipe-book-authorisation/src/main/java/com/recipebook/recipebook/model/Role.java
@@ -0,0 +1,28 @@
+package com.recipebook.recipebook.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role_details")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "role_name")
+    private String name;
+
+    @ManyToMany(mappedBy = "role")
+    private Set<User> users;
+}
