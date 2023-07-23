44095d46825902de9b3c0bbe4e92878e382c1850
priya4991 <pdutta04@gmail.com>
2023-07-24 00:32:23 +0100

    more integration tests

--- src/test/java/com/authservice/jwtauth/integration/UserIntegrationTest.java
+++ src/test/java/com/authservice/jwtauth/integration/UserIntegrationTest.java
@@ -1,28 +1,29 @@
 package com.authservice.jwtauth.integration;
 
 import java.time.LocalDate;
+import java.util.Collections;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.authservice.jwtauth.model.Role;
+import com.authservice.jwtauth.model.User;
 import com.authservice.jwtauth.model.DTO.SignupDTO;
-import com.authservice.jwtauth.service.CrudUserService;
+import com.authservice.jwtauth.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
-@ExtendWith( SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
@@ -32,15 +33,28 @@ public class UserIntegrationTest {
     private MockMvc mockMvc; 
     @Autowired
     private ResourceLoader resourceLoader;
+    @Autowired 
+    private PasswordEncoder passwordEncoder;
     @Autowired
-    private CrudUserService crudUserService;
+    private UserRepository userRepository;
+
+    private static final String BEARERTOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZHV0dGFAZ21haWwuY29tIiwiZXhwIjoxNjkwMTcwNTMyLCJpYXQiOjE2OTAxNTI1NzN9.Eip7Of0RlKZPnNA1PDDs0jp02kyqyoMllY8jApeA3qQ";
 
     ///BeforeEach does not work with JUnit 4 @RunWith(SpringRunner.class)
     ///cannot use JUnit4 and JUnit 5 annotations together
     @BeforeEach   
     public void setup() {
-        SignupDTO signupDTO = createSignupDTO();
-        crudUserService.createUser(signupDTO);
+        SignupDTO signup = createSignupDTO();
+        User user = new User();
+        user.setUsername(signup.getUsername());
+        user.setFirstName(signup.getFirstname());
+        user.setLastName(signup.getLastname());
+        user.setDateOfBirth(signup.getDob());
+        user.setEmail(signup.getEmail());
+        user.setPhoneNumber(signup.getPhone());
+        user.setPassword(passwordEncoder.encode(signup.getPassword()));
+        user.setRoles(Collections.singleton(new Role("ROLE_USER")));
+        userRepository.save(user);
     }
 
     @Test
@@ -78,9 +92,36 @@ public class UserIntegrationTest {
     }
 
     @Test
-    public void signin_success_validCreds() {
-        
+    public void signin_success_validCreds() throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:signinrequest.json");
+        String content = new String(resource.getInputStream().readAllBytes());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signin")
+        .content(content)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    public void signin_badRequest_invalidCreds() throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:signinrequest_invalid.json");
+        String content = new String(resource.getInputStream().readAllBytes());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signin")
+        .content(content)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    // @Test
+    // public void changePassword_success_validRequest() throws Exception {
+    //     Resource resource = resourceLoader.getResource("classpath:changePassword.json");
+    //     String content = new String(resource.getInputStream().readAllBytes());
+    //     mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/changepassword/1")
+    //     .content(content)
+    //     .header("Authorization", BEARERTOKEN)
+    //     .contentType(MediaType.APPLICATION_JSON))
+    //     .andExpect(MockMvcResultMatchers.status().isOk())
+    //     .andExpect(MockMvcResultMatchers.content().string("Password changed successfully"));
+    // }
 
    
     private SignupDTO createSignupDTO() {
