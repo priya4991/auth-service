7aaef9091dfbe1817dda81a212fbeb582870c19b
priya4991 <pdutta04@gmail.com>
2024-01-26 20:15:08 +0000

    fixed cors issue

--- src/main/java/com/authservice/jwtauth/controller/AuthController.java
+++ src/main/java/com/authservice/jwtauth/controller/AuthController.java
@@ -5,14 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.authservice.jwtauth.model.User;
 import com.authservice.jwtauth.model.Auth.TokenResponse;
@@ -24,6 +17,7 @@ import com.authservice.jwtauth.service.CrudUserService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
